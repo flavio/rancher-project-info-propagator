eca9e8bd205d7aa11949fab05d1c53359f3a5a70
Flavio Castelli <fcastelli@suse.com>
2023-05-10 15:16:52 +0200

    refactor: change cli env variable names
    Use a prefix to avoid clashes with the env variables added by Kubernetes
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,7 @@ pub struct Cli {
     /// Log level
     #[arg(
         long,
-        env,
+        env = "PROPAGATOR_LOG_LEVEL",
         default_value_t = LevelFilter::INFO,
         value_parser = clap::builder::PossibleValuesParser::new(["trace", "debug", "info", "warn", "error"])
             .map(|s| s.parse::<LevelFilter>().unwrap()),
@@ -16,16 +16,26 @@ pub struct Cli {
     pub log_level: LevelFilter,
 
     /// ID of the cluster. To be used when deployed inside of a downstream cluster
-    #[clap(long, env, required(false), requires = "kubeconfig_upstream")]
+    #[clap(
+        long,
+        env = "PROPAGATOR_CLUSTER_ID",
+        required(false),
+        requires = "kubeconfig_upstream"
+    )]
     pub cluster_id: Option<String>,
 
     /// Path to the kubeconfig file used to connect to the upstream cluster. To be used when
     /// deployed inside of a downstream cluster
-    #[clap(long, env, required(false), requires = "cluster_id")]
+    #[clap(
+        long,
+        env = "PROPAGATOR_KUBECONFIG_UPSTREAM",
+        required(false),
+        requires = "cluster_id"
+    )]
     pub kubeconfig_upstream: Option<std::path::PathBuf>,
 
     /// Path where the sqlite database is going to be saved
     /// Required when the controller is deployed inside of a downstream cluster
-    #[clap(long, env, required(false), default_value_t = String::from("."))]
+    #[clap(long, env = "PROPAGATOR_DATA_PATH", required(false), default_value_t = String::from("."))]
     pub data_path: String,
 }
